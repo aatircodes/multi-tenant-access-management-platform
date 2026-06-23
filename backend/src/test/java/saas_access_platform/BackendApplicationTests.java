50b4f94365a7021aa643b1ef66f98b2573d1b5e1
aatircodes <aatirmirza999@gmail.com>
2026-06-23 15:13:26 +0530

    chore: initialize project with Spring Boot, MySQL config, and root gitignore

---
+++ backend/src/test/java/saas_access_platform/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package saas_access_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
