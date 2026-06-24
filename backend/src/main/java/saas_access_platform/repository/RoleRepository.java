3ec71e8465d16fd2ee97c2afcd95a40c7d7c821e
aatircodes <aatirmirza999@gmail.com>
2026-06-24 13:16:43 +0530

    feat: add RoleRepository

---
+++ backend/src/main/java/saas_access_platform/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package saas_access_platform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import saas_access_platform.entity.Role;
+import java.util.List;
+import java.util.Optional;
+
+public interface RoleRepository
+        extends JpaRepository<Role, Long> {
+
+    List<Role> findAllByOrgId(Long orgId);
+    Optional<Role> findByNameAndOrgId(String name, Long orgId);
+    boolean existsByNameAndOrgId(String name, Long orgId);
+}
