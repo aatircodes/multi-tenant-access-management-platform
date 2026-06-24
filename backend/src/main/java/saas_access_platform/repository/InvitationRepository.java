0d7897a326544a31b0382afd36970f408d5ce040
aatircodes <aatirmirza999@gmail.com>
2026-06-24 13:40:09 +0530

    feat: add InvitationRepository

---
+++ backend/src/main/java/saas_access_platform/repository/InvitationRepository.java
@@ -0,0 +1,15 @@
+package saas_access_platform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import saas_access_platform.entity.Invitation;
+import java.util.Optional;
+import java.util.List;
+
+public interface InvitationRepository
+        extends JpaRepository<Invitation, Long> {
+
+    Optional<Invitation> findByToken(String token);
+    List<Invitation> findAllByOrgId(Long orgId);
+    boolean existsByEmailAndOrgId(String email, Long orgId);
+    Optional<Invitation> findByEmailAndOrgId(String email, Long orgId);
+}
