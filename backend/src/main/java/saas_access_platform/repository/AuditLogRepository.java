b0effddcd3311befc71bd02c1c51681e1eb44afc
aatircodes <aatirmirza999@gmail.com>
2026-06-24 13:54:57 +0530

    feat: add AuditLogRepository

---
+++ backend/src/main/java/saas_access_platform/repository/AuditLogRepository.java
@@ -0,0 +1,15 @@
+package saas_access_platform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import saas_access_platform.entity.AuditLog;
+import java.util.List;
+
+public interface AuditLogRepository
+        extends JpaRepository<AuditLog, Long> {
+
+    List<AuditLog> findAllByOrgIdOrderByTimestampDesc(Long orgId);
+    List<AuditLog> findAllByOrgIdAndActorUserIdOrderByTimestampDesc(
+            Long orgId, Long actorUserId);
+    List<AuditLog> findAllByOrgIdAndEntityTypeOrderByTimestampDesc(
+            Long orgId, String entityType);
+}
