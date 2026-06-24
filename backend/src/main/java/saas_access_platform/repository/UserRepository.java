dbaefe4eda3643b5be1926d13fe8621b64bf3030
aatircodes <aatirmirza999@gmail.com>
2026-06-24 13:12:54 +0530

    feat: add UserRepository

---
+++ backend/src/main/java/saas_access_platform/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package saas_access_platform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import saas_access_platform.entity.User;
+import java.util.Optional;
+
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailAndOrgId(String email, Long orgId);
+    boolean existsByEmailAndOrgId(String email, Long orgId);
+}
