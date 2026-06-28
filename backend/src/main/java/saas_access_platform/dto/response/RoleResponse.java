45bfb6066c23163a90f75b88ed6c9fe5ff4e9fe8
aatircodes <aatirmirza999@gmail.com>
2026-06-28 12:49:29 +0530

    feat(rbac): add RoleResponse DTO

---
+++ backend/src/main/java/saas_access_platform/dto/response/RoleResponse.java
@@ -0,0 +1,14 @@
+package saas_access_platform.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class RoleResponse {
+    private Long id;
+    private String name;
+    private Long orgId;
+    private LocalDateTime createdAt;
+}
