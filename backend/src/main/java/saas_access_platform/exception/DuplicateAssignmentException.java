2b4cb12e2316f52e5acbf6c39515e9cd3ef6c56b
aatircodes <aatirmirza999@gmail.com>
2026-06-28 13:24:22 +0530

    feat(rbac): add DuplicateAssignmentException

---
+++ backend/src/main/java/saas_access_platform/exception/DuplicateAssignmentException.java
@@ -0,0 +1,7 @@
+package saas_access_platform.exception;
+
+public class DuplicateAssignmentException extends RuntimeException {
+    public DuplicateAssignmentException(String message) {
+        super(message);
+    }
+}
