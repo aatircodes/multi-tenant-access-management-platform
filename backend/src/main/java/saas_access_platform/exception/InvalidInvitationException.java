ee294f6702d9fb75c69f1f4fac2d88df0040e121
aatircodes <aatirmirza999@gmail.com>
2026-06-28 19:46:35 +0530

    feat: add InvalidInvitationException

---
+++ backend/src/main/java/saas_access_platform/exception/InvalidInvitationException.java
@@ -0,0 +1,7 @@
+package saas_access_platform.exception;
+
+public class InvalidInvitationException extends RuntimeException {
+    public InvalidInvitationException(String message) {
+        super(message);
+    }
+}
