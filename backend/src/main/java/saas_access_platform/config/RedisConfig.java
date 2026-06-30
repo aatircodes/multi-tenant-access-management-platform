f069944870921887d2800ad3524006512deaf768
aatircodes <aatirmirza999@gmail.com>
2026-06-30 11:02:14 +0530

    Add RedisConfig with RedisTemplate and Lua script bean

---
+++ backend/src/main/java/saas_access_platform/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package saas_access_platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.List;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public DefaultRedisScript<List> slidingWindowScript() {
+        DefaultRedisScript<List> script = new DefaultRedisScript<>();
+        script.setLocation(new ClassPathResource("scripts/sliding_window.lua"));
+        script.setResultType(List.class);
+        return script;
+    }
+}
