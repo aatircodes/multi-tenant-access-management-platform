645a449f3fc1726d196c976ad9c8129660c703c7
aatircodes <aatirmirza999@gmail.com>
2026-07-02 11:52:59 +0530

    Add findAllByOrgId to UserRepository

--- backend/src/main/java/saas_access_platform/repository/UserRepository.java
+++ backend/src/main/java/saas_access_platform/repository/UserRepository.java
@@ -2,6 +2,7 @@ package saas_access_platform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import saas_access_platform.entity.User;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository
@@ -10,4 +11,5 @@ public interface UserRepository
     Optional<User> findByEmailAndOrgId(String email, Long orgId);
     boolean existsByEmailAndOrgId(String email, Long orgId);
     Optional<User> findByIdAndOrgId(Long id, Long orgId);
+    List<User> findAllByOrgId(Long orgId);
 }
