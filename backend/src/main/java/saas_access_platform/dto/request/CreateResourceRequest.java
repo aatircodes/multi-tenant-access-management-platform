34e35f32c9400a2869bd6843c3ac8af61c6dc3a2
aatircodes <aatirmirza999@gmail.com>
2026-07-02 13:23:21 +0530

    Add optional description field to resource DTOs

--- backend/src/main/java/saas_access_platform/dto/request/CreateResourceRequest.java
+++ backend/src/main/java/saas_access_platform/dto/request/CreateResourceRequest.java
@@ -10,4 +10,7 @@ public class CreateResourceRequest {
     @NotBlank(message = "Resource name is required")
     @Size(min = 2, max = 100, message = "Resource name must be between 2 and 100 characters")
     private String name;
+
+    @Size(max = 500, message = "Description must be under 500 characters")
+    private String description;
 }
