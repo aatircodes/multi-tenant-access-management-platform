cc1d24962d88f7bb1571a3251078ed71d8a7f6bf
aatircodes <aatirmirza999@gmail.com>
2026-07-02 13:40:32 +0530

    Add paginated findAllByOrgId to AuditLogRepository

--- backend/src/main/java/saas_access_platform/repository/AuditLogRepository.java
+++ backend/src/main/java/saas_access_platform/repository/AuditLogRepository.java
@@ -1,5 +1,7 @@
 package saas_access_platform.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import saas_access_platform.entity.AuditLog;
 import java.util.List;
@@ -8,6 +10,7 @@ public interface AuditLogRepository
         extends JpaRepository<AuditLog, Long> {
 
     List<AuditLog> findAllByOrgIdOrderByTimestampDesc(Long orgId);
+    Page<AuditLog> findAllByOrgId(Long orgId, Pageable pageable);
     List<AuditLog> findAllByOrgIdAndActorUserIdOrderByTimestampDesc(
             Long orgId, Long actorUserId);
     List<AuditLog> findAllByOrgIdAndEntityTypeOrderByTimestampDesc(
